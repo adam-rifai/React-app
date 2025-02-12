ae944abee4e26aaa9b9dcfb86d310baf55f12ea5
rifai <alliiyassir@gmail.com>
2025-02-12 23:29:11 +0100

    App-version 1.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => alert("you've changed the counter"), [counter]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCounter((prev) => Math.max(0, prev - 1))}>
+        -
+      </button>
+
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
     </div>
   );
-}
+};
 
 export default App;
